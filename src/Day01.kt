470b4b25de5565f68c297582260bc71df95c944d
Julien Lengrand-Lambert <julien@lengrand.fr>
2024-12-27 22:46:02 +0100

    Finishes Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -15,24 +15,36 @@ fun main() {
         firsts.sort()
         seconds.sort()
 
-        println(firsts)
-        println(seconds)
-
-        val combo = firsts.zip(seconds){ f, s -> s - f }
-
-        return combo.sumOf { abs(it) }
+        return firsts
+            .zip(seconds){ f, s -> s - f }
+                .sumOf { abs(it) }
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val firsts = mutableListOf<Int>()
+        val seconds = mutableListOf<Int>()
+
+        input.forEach {
+            val split = it.split(" ")
+            firsts.add(split.first().toString().toInt())
+            seconds.add(split.last().toString().toInt())
+        }
+
+        firsts.sort()
+        seconds.sort()
+
+        return firsts.map { n ->
+                seconds.filter { n == it }.size * n
+            }
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 11)
-//    check(part2(testInput) == 5)
+    check(part2(testInput) == 31)
 
     val input = readInput("Day01")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
