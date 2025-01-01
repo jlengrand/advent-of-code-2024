55419ff7e01024baad8870651b58b0ae66026014
Julien Lengrand-Lambert <julien@lengrand.fr>
2025-01-02 00:20:05 +0100

    day 4 no fun

---
+++ src/Day04.kt
@@ -0,0 +1,37 @@
+fun main() {
+
+    fun findHorizontal(input: List<String>) : Int {
+
+        var finalCount = 0
+        for (line in input) {
+            val matches = Regex("XMAS").findAll(line)
+            val matchesReversed = Regex("XMAS").findAll(line.reversed())
+            finalCount += matches.count() + matchesReversed.count()
+        }
+
+        println(finalCount)
+        return finalCount
+    }
+
+    fun findVertical(input: List<String>) : Int {
+
+    }
+
+    fun part1(input: List<String>): Int {
+
+        return findHorizontal(input)
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 7)
+//    check(part2(testInput) == 5)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
