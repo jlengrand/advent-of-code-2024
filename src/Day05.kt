6b19db18b4598d0050ec74e3c764c41c3cee2ad3
Julien Lengrand-Lambert <julien@lengrand.fr>
2025-01-02 23:36:29 +0100

    Cleanup part 1

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,22 +1,49 @@
 fun main() {
 
+    fun isPairValid(pair: Pair<Int, Int>, update : List<Int>) : Boolean{
+        return update.indexOf(pair.second) > update.indexOf(pair.first)
+    }
+
     fun isValid(pairs: List<Pair<Int, Int>>, update: List<Int>) : Boolean {
+        val relevantPairs = pairs.filter { pair -> update.contains(pair.second) && update.contains(pair.first) }
+        return relevantPairs.all { isPairValid(it, update) }
+    }
+
+    fun reorder(pairs: List<Pair<Int, Int>>, update: List<Int>) : List<Int> {
+        val relevantPairs = pairs.filter { pair -> update.contains(pair.second) && update.contains(pair.first) }
+
+        val movableUpdate = update.toMutableList()
+
+
+        while(!isValid(pairs, movableUpdate)){
+
+            for(pair in relevantPairs){
+
+            }
+        }
 
         for(v in update){
             val relevantPairs = pairs.filter { pair -> pair.first == v }
+
+
             for (pair in relevantPairs) {
                 val after = pair.second
 
                 val indexV = update.indexOf(v)
                 val indexS = update.indexOf(after)
 
-                if(indexS != -1 && indexV > indexS) return false
+                if(indexS != -1 && indexV > indexS) {
+                    movableUpdate[indexV] = pair.second
+                    movableUpdate[indexS] = pair.first
+                }
 
             }
+
         }
 
-        return true
+        return movableUpdate
     }
+
 
     fun part1(input: List<String>): Int {
 
@@ -33,24 +60,41 @@ fun main() {
             }
         }
 
-        val midItems = updates
+        return updates
             .filter { isValid(pairs, it) }
-            .map{ update -> update[update.size / 2] }
-
-
-        return midItems.sum()
+            .sumOf { update -> update[update.size / 2] }
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val pairs = mutableListOf<Pair<Int, Int>>()
+        val updates = mutableListOf<List<Int>>()
+
+        for (line in input) {
+            if(line.contains("|")){
+                val pages = line.split("|")
+                pairs.add(Pair(pages[0].toInt(), pages[1].toInt()))
+            }
+            else if(line.contains(",")){
+                updates.add(line.split(",").map { it.toInt() })
+            }
+        }
+
+        val incorrect = updates.filter { !isValid(pairs, it) }
+
+        // TODO : Reorder here
+        val reordered = incorrect.map { reorder(pairs, it) }
+
+        return reordered
+            .sumOf { update -> update[update.size / 2] }
+
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 143)
-//    check(part2(testInput) == 5)
+//    check(part2(testInput) == 123)
 
     val input = readInput("Day05")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
