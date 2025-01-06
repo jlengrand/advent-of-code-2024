64bf256c1ae9e510ef4264da2031db9e368cbc82
Julien Lengrand-Lambert <julien@lengrand.fr>
2025-01-06 20:00:19 +0100

    Solves part 1 Day 6

---
+++ src/Day06.kt
@@ -0,0 +1,157 @@
+import javax.swing.text.Position
+
+enum class Direction {
+    UP, DOWN, RIGHT, LEFT
+}
+
+fun main() {
+
+    fun printMap(input: List<String>, obstacles : List<Pair<Int, Int>>, visited : List<Pair<Int, Int>>, position : Pair<Int, Int>, direction : Direction) {
+        val depth = input.size
+        val length = input[0].length
+
+        for(i in 0 until depth -1){
+            val line = input[i].toMutableList()
+            for (j in 0 until length - 1){
+                if (position == Pair(i, j)) {
+                    if(direction == Direction.UP) { line[j] = '^' }
+                    if(direction == Direction.DOWN) { line[j] = 'v' }
+                    if(direction == Direction.LEFT) { line[j] = '<' }
+                    if(direction == Direction.RIGHT) { line[j] = '>' }
+                }
+                else if (visited.contains(Pair(i, j))) {
+                    line[j] = 'X'
+                }
+                else if (obstacles.contains(Pair(i, j))) {
+                    line[j] = '#'
+                }
+                else {
+                    line[j] = '.'
+                }
+            }
+            println(line.joinToString(""))
+        }
+    }
+
+    fun insideMap(position: Pair<Int, Int>, mapSize : Pair<Int, Int>): Boolean {
+        val insideRow = position.first >= 0  && position.first < mapSize.first
+        val insideCol = position.second >= 0  && position.second < mapSize.second
+
+        return insideRow && insideCol
+    }
+
+    fun part1(input: List<String>): Int {
+
+        val obstacles = mutableListOf<Pair<Int, Int>>()
+        val visited = mutableListOf<Pair<Int, Int>>()
+
+        var position = Pair(-1, -1)
+        var direction = Direction.UP
+        var mapSize = Pair(input[0].length, input.size)
+
+        var idxCol = 0
+        for (row in input){
+            var idxRow = 0
+
+            for (j in row){
+                if(j == '#'){
+                    obstacles.add(Pair(idxCol, idxRow))
+                }
+
+                if(j == '^'){
+                    position = Pair(idxCol, idxRow)
+                    direction = Direction.UP
+                }
+                if(j == 'v'){
+                    position = Pair(idxCol, idxRow)
+                    direction = Direction.DOWN
+                }
+                if(j == '<'){
+                    position = Pair(idxCol, idxRow)
+                    direction = Direction.LEFT
+                }
+                if(j == '>'){
+                    position = Pair(idxCol, idxRow)
+                    direction = Direction.RIGHT
+                }
+                idxRow += 1
+            }
+
+            idxCol +=1
+        }
+
+//        println("-----")
+//        println(mapSize)
+//        println(obstacles)
+//        println(position)
+//        println(direction)
+//        println("-----")
+//        printMap(input, obstacles, visited, position, direction)
+//        println("-----")
+
+        // LOOP
+        while(insideMap(position, mapSize)){
+//            println("-----")
+//            println(position)
+//            printMap(input, obstacles, visited, position, direction)
+
+            if(direction == Direction.UP){
+                visited.add(position)
+                val newPosition = Pair(position.first - 1, position.second)
+                if (obstacles.contains(newPosition)){
+                    direction = Direction.RIGHT
+                }
+                else{
+                    position = newPosition
+                }
+            }
+            else if(direction == Direction.RIGHT){
+                visited.add(position)
+                val newPosition = Pair(position.first, position.second + 1)
+                if (obstacles.contains(newPosition)){
+                    direction = Direction.DOWN
+                }
+                else{
+                    position = newPosition
+                }
+            }
+            else if(direction == Direction.DOWN){
+                visited.add(position)
+                val newPosition = Pair(position.first + 1, position.second)
+                if (obstacles.contains(newPosition)){
+                    direction = Direction.LEFT
+                }
+                else{
+                    position = newPosition
+                }
+            }
+            else{ // DIRECTION LEFT
+                visited.add(position)
+                val newPosition = Pair(position.first, position.second - 1)
+                if (obstacles.contains(newPosition)){
+                    direction = Direction.UP
+                }
+                else{
+                    position = newPosition
+                }
+            }
+        }
+
+        val visitedUnique = visited.toSet().toList()
+
+        return visitedUnique.size
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test")
+    check(part1(testInput) == 41)
+//    check(part2(testInput) == 5)
+
+    val input = readInput("Day06")
+    println(part1(input))
+//    println(part2(input))
+}
